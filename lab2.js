4bc86e80f55ae8f6483ec0e5b87b729c2d25f793
Jana <janaakamaal2004@gmail.com>
2025-01-29 11:58:06 +0200

    Create lab2.js
    create simple regular expressions.

---
+++ lab2.js
@@ -0,0 +1,6 @@
+//Create a regular expression, for use in ECMAScript (JavaScript), that searches for the term "cat" anywhere in a string.
+cat
+
+//Create a regular expression, for use in ECMAScript (JavaScript), that searches for one or more "A" followed by one or more "B".
+A+B+
+
